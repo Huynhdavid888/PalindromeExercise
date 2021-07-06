9681be58e5704b7f1d34bd6d3a28a65baf2db97b
Huynhdavid888 <huynhdavid1245@gmail.com>
2021-07-05 21:46:49 -0700

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public WordSmith()
+        {
+
+        }
+
+    public bool isAPalindrome(string input)
+        {
+            int length = input.Length;
+            for (int i = 0; i < length / 2; i++)
+            {
+                if (char.ToLower(input[i]) != char.ToLower(input[length - i - 1]))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,18 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("tenet", true)]
+        [InlineData("map", false)]
+        [InlineData("honda", false)]
+        public void Test1(string word, bool expected)
         {
+            var wordsmith = new WordSmith();
 
+            var actual = wordsmith.isAPalindrome(word);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
